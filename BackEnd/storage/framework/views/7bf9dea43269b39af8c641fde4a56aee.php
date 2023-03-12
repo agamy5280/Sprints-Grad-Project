e26ec553676180ae68ca8204e02333bf1f0d2b6e
agamy5280 <marawan.agamy2010@hotmail.com>
2023-03-13 01:48:21 +0200

    Revert "Added Backend"
    This reverts commit 07873e32b1296415aa19e2dee97f90d5074a087f.

--- BackEnd/storage/framework/views/7bf9dea43269b39af8c641fde4a56aee.php
+++
@@ -1,7 +0,0 @@
-<h2>Hello!</h2>
-
-<p>Please verify your email by visiting this URL:</p>
-
-<a href="<?php echo e($URL); ?>">Activation Link</a>
-
-<p>Thank you!</p><?php /**PATH C:\Users\maraw\Desktop\gradProject-FrontEnd\MoneyWall-BackEnd\MoneyWall-Backend\resources\views/email/Email-Verification.blade.php ENDPATH**/ ?>
